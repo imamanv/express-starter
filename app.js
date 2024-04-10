d7c2c6d3619fc3c323a8a9f7b757cafee013bf5b
imamanv <56167773+imamanv@users.noreply.github.com>
2024-04-11 00:46:49 +0530

    added basic express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Home Page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About Page</h1>");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on Port 3000");
+});
