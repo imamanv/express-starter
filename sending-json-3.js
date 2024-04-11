fec7a2e1ccc6841ed2c136a6dfafb9d1d3c6b006
imamanv <56167773+imamanv@users.noreply.github.com>
2024-04-11 15:12:38 +0530

    Update sending-json-3.js

--- sending-json-3.js
+++ sending-json-3.js
@@ -2,8 +2,41 @@ const express = require("express");
 
 const app = express();
 const { products } = require("./data");
-app.get("/", (req, res) => {
-  res.json(products);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, price } = product;
+    return { id, name, price };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productId", (req, res) => {
+  const product = products.find((product) => {
+    // req.params returns all the route parameters we mention using :
+    return product.id === +req.params.productId;
+  });
+  if (!product) {
+    res.status(404).send("<h1>Product doesn't exist</h1>");
+  } else {
+    res.json(product);
+  }
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let productsCopy = [...products];
+  if (search) {
+    productsCopy = productsCopy.filter((product) =>
+      product.name.startsWith(search)
+    );
+  }
+  if (limit) {
+    productsCopy = productsCopy.slice(0, +limit);
+  }
+  if (productsCopy.length < 1) {
+    return res.status(200).send({ success: true, data: productsCopy });
+  }
+  res.json(productsCopy);
 });
 
 app.listen(3000, () => {
